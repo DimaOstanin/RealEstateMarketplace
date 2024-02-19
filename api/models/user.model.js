4c2e2f3a262403e3d34ee97fc04c304dbe581483
DimaOstanin <xnadab@gmail.com>
2024-02-19 21:47:10 +0200

    update the header and make profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
         require:true,
         
     },
-    avata:{
+    avatar:{
         type:String,
         default:"https://www.google.com/url?sa=i&https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fprofile&psig=AOvVaw0_EVLQHzDWYj5UK4QVkoNW&ust=1706503732203000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDWg_yj_4MDFQAAAAAdAAAAABAD"
     }
