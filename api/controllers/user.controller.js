0da7740de7a29fa7a9d3d7fca386d8a258517915
DimaOstanin <xnadab@gmail.com>
2024-02-27 16:40:57 +0200

    get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -47,5 +48,23 @@ export const updateUser = async (req, res, next) => {
       res.status(200).json('User has been deleted!');
     } catch (error) {
       next(error);
+    }
+  }
+
+  export const getUserListings = async(req,res,next) =>{
+    if(req.user.id === req.params.id){
+      try {
+        const listings = await Listing.find({userRef:req.params.id});
+        res.status(200).json(listings);
+      } catch (error) {
+        
+      }
+    }else{
+      return next(errorHandler(401,'You can only view you own listings!'));
+    }
+    try {
+      
+    } catch (error) {
+      
     }
   }
